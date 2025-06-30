f768bf74e4dd6a844418c9c14e5dde24bc4c7c91
Zayduni Hilasaca Dev <zayhilasari@gmail.com>
2025-06-29 20:18:34 -0400

    Proyecto listo para despliegue

--- backend/server.js
+++ backend/server.js
@@ -26,14 +26,8 @@ app.use('/api/', limiter);
 
 // PostgreSQL pool
 const pool = new Pool({
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'mapa_turistico_lapaz',
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'ari_2025',
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000
+  connectionString: process.env.DATABASE_URL, // Usa la URL completa
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
 pool.on('error', (err) => {
